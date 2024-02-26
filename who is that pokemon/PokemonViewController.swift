998137b34f2d43648d5b7bd820e33a2788e13e14
AndresCR24 <c-andres-98@hotmail.com>
2024-02-26 17:40:52 -0500

    Inicial comit

--- who is that pokemon/PokemonViewController.swift
+++ who is that pokemon/PokemonViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class PokemonViewController: UIViewController {
 
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var labelMesagge: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for button in answerButtons {
+            button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+            button.layer.shadowOpacity = 1.0
+            button.layer.shadowRadius = 0
+            button.layer.masksToBounds = false
+            button.layer.cornerRadius = 10.0
+        }
     }
     
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
 }
